5ab3af0646f13df74a6cb0e27c3897b607dab66b
joaoffnascimento <thecurrentuser@live.com>
2021-02-27 15:31:20 -0300

    feat: new mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace starting_with_aspnetcore3._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
