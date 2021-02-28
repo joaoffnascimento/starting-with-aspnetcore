ab8b52c6c5cb291ebee542c8c7974d1a554a7e47
joaoffnascimento <thecurrentuser@live.com>
2021-02-28 13:59:28 -0300

    chore(DbSetup): add Dbset for entities

--- Data/DbSetup.cs
+++ Data/DbSetup.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using starting_with_aspnetcore.Models;
 
 namespace starting_with_aspnetcore.Data
 {
     public class DbSetup : DbContext
     {
+        public DbSet<Tech> Tech { get; set; }
+        public DbSet<Repository> Repository { get; set; }
         public DbSetup(DbContextOptions<DbSetup> options) : base(options)
         {
         }
