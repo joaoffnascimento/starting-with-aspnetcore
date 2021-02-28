19cddc68c5d55d9c80194ccefb7b00d9778b388f
joaoffnascimento <thecurrentuser@live.com>
2021-02-28 11:55:38 -0300

    feat(Data): create and configure context

--- Data/DbSetup.cs
+++ Data/DbSetup.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace starting_with_aspnetcore.Data
 {
     public class DbSetup : DbContext
     {
-        
+        public DbSetup(DbContextOptions<DbSetup> options) : base(options)
+        {
+        }
     }
 }
