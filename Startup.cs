ed3346a05cd5fd92c676d1a8d4b4d1becaaf56db
joaoffnascimento <thecurrentuser@live.com>
2021-02-28 16:43:48 -0300

    chore(Startup): add fluentvalidation on startup

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,8 @@ namespace starting_with_aspnetcore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().AddFluentValidation(fvc => fvc.RegisterValidatorsFromAssemblyContaining<Startup>());
+
             services.AddDbContext<DbSetup>(options => options.UseSqlServer(Configuration.GetConnectionString("sqlserver")));
 
             services.AddControllersWithViews();
