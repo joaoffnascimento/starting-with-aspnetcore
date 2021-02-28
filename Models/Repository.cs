1532d33b3108ca8e80e7c01d51b1c3dd67177b4d
joaoffnascimento <thecurrentuser@live.com>
2021-02-28 13:59:04 -0300

    feat(Repository): add repository

---
+++ Models/Repository.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace starting_with_aspnetcore.Models
+{
+    public class Repository : Entity
+    {
+        public string Title { get; set; }
+        public ICollection<Tech> Techs { get; set; }
+        public int Stars { get; set; }
+        public string About { get; set; }
+    }
+}
